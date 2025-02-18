2dfe4f2da329213492c19e3b9699029fc58cc520
helixaflux <helixaflux@gmail.com>
2025-02-18 16:59:32 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+    el: "#background-wrapper", // Apply the effect to the background wrapper
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1,
+    scaleMobile: 1.00,
+    color: 0xff00ff, // Magenta color
+    points: 8.00,
+    maxDistance: 23.00,
+    spacing: 20.00
+  });
